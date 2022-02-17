7b4ea3732afda83048fb1ed6c5278eec6125c8ef
atanasgegov <a_gegov@abv.bg>
2022-02-17 18:50:53 +0200

    Spring Boot App Setup

---
+++ src/main/java/com/akg/data/perf/comparisons/ComparisonsApplication.java
@@ -0,0 +1,13 @@
+package com.akg.data.perf.comparisons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparisonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparisonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akg/data/perf/comparisons/ComparisonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akg.data.perf.comparisons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparisonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
